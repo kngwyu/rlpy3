012e73256aefcdd8fc7031264e6278dc159d043b
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-10-20 13:38:32 +0900

    [PSRL] Faster value iteration

--- rlpy/agents/_vi_impl.py
+++ rlpy/agents/_vi_impl.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 def compute_q_values(r, p, max_steps, discount, convergence_threshold=0.05):
+    """Compute Q values of given MDP(r, p, discount) by Value Iteration.
+    """
     n_states, n_actions = r.shape
     q = np.zeros_like(r)
     v = np.zeros(r.shape[0])
@@ -9,8 +11,7 @@ def compute_q_values(r, p, max_steps, discount, convergence_threshold=0.05):
     for i in range(max_steps):
         v_old = v.copy()
         for s in range(n_states):
-            for a in range(n_actions):
-                q[s, a] = r[s, a] + discount * np.dot(p[s, a], v)
+            q[s] = r[s] + discount * p[s].dot(v)
             v[s] = q[s].max()
         diff = np.abs(v_old - v).mean()
         if diff < convergence_threshold:

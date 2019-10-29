24b0fd1c761ed33eca510ea34af0ca72496573e2
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-10-29 20:03:04 +0900

    Move mdpsolve experiments into examples/mdp-solvers

---
+++ examples/mdp-solvers/ar_gridworld.py
@@ -0,0 +1,41 @@
+import click
+from rlpy.domains import AnyRewardGridWorld
+from rlpy.mdp_solvers import (
+    TrajectoryBasedPolicyIteration,
+    TrajectoryBasedValueIteration,
+    PolicyIteration,
+    ValueIteration,
+)
+from rlpy.representations import Tabular
+from rlpy.tools.cli import run_mb_experiment
+
+
+def select_domain(map_, **kwargs):
+    map_ = AnyRewardGridWorld.default_map(map_ + ".txt")
+    return AnyRewardGridWorld(map_, random_start=True, noise=0.1)
+
+
+def select_agent(name, domain, seed, **kwargs):
+    name = None if name is None else name.lower()
+    tabular = Tabular(domain, discretization=20)
+    if name is None or name == "vi":
+        return ValueIteration(seed, tabular, domain)
+    elif name == "pi":
+        return PolicyIteration(seed, tabular, domain)
+    elif name in ["tpi", "traj-pi"]:
+        return TrajectoryBasedPolicyIteration(seed, tabular, domain)
+    elif name in ["tvi", "traj-vi"]:
+        return TrajectoryBasedValueIteration(seed, tabular, domain)
+    else:
+        raise ValueError("{} is not supported".format(name))
+
+
+if __name__ == "__main__":
+    run_mb_experiment(
+        select_domain,
+        select_agent,
+        other_options=[
+            click.Option(["--map", "map_"], type=str, default="6x6guided"),
+            click.Option(["--noise"], type=float, default=0.1),
+        ],
+    )

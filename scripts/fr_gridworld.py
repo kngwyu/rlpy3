dcd302411d5e45c412789d780bcaf6c5e66e40c8
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-12-17 16:30:16 +0900

    Renew experiment scripts

---
+++ scripts/fr_gridworld.py
@@ -0,0 +1,24 @@
+import subprocess
+
+MAPS = ["6x6guided", "6x6sparse", "6x6liar", "6x6spelunky", "6x6spenalty10"]
+
+if __name__ == "__main__":
+    for map_ in MAPS:
+        path = "../examples/fr_gridworld.py"
+        logdir = "Results/{}-ep01".format(map_)
+        subprocess.run(
+            [
+                "pipenv",
+                "run",
+                "python",
+                path,
+                "--logdir",
+                logdir,
+                "--map",
+                map_,
+                "--num-policy-checks=20",
+                "--max-steps=4000",
+                "--epsilon=0.1",
+                "--plot-save",
+            ]
+        )
